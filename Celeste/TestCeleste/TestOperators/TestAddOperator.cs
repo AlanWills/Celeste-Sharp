9a70d9c47e5b122a1f4f053f98c7c240f10e2bae
AlanWills <alawills@googlemail.com>
2016-05-30 19:37:34 +0100

    Trying to get more complex adding working

--- Celeste/TestCeleste/TestOperators/TestAddOperator.cs
+++ Celeste/TestCeleste/TestOperators/TestAddOperator.cs
@@ -7,47 +7,47 @@ namespace TestCeleste
     [TestClass]
     public class TestAddOperator : CelesteUnitTest
     {
-        //[TestMethod]
-        //public void TestAddOperatorAddNumbers()
-        //{
-        //    CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorNumbers.cel");
-        //    script.Run();
+        [TestMethod]
+        public void TestAddOperatorAddNumbers()
+        {
+            CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorNumbers.cel");
+            script.Run();
 
-        //    CelesteTestUtils.CheckStackSize(2);
-        //    CelesteTestUtils.CheckStackResult(10.0f);
-        //    CelesteTestUtils.CheckStackResult(15.0f);
-        //}
+            CelesteTestUtils.CheckStackSize(2);
+            CelesteTestUtils.CheckStackResult(10.0f);
+            CelesteTestUtils.CheckStackResult(15.0f);
+        }
 
-        //[TestMethod]
-        //public void TestAddOperatorAddStrings()
-        //{
-        //    CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorStrings.cel");
-        //    script.Run();
+        [TestMethod]
+        public void TestAddOperatorAddStrings()
+        {
+            CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorStrings.cel");
+            script.Run();
 
-        //    CelesteTestUtils.CheckStackSize(1);
-        //    CelesteTestUtils.CheckStackResult("testadding");
-        //}
+            CelesteTestUtils.CheckStackSize(1);
+            CelesteTestUtils.CheckStackResult("testadding");
+        }
 
-        //[TestMethod]
-        //public void TestAddOperatorAddLists()
-        //{
-        //    CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorLists.cel");
-        //    script.Run();
+        [TestMethod]
+        public void TestAddOperatorAddLists()
+        {
+            CelesteScript script = new CelesteScript("TestScripts\\Operators\\Add\\TestAddOperatorLists.cel");
+            script.Run();
 
-        //    CelesteTestUtils.CheckStackSize(0);
+            CelesteTestUtils.CheckStackSize(0);
 
-        //    List<object> expected = new List<object>()
-        //    {
-        //        5.0f,
-        //        "Test",
-        //        true,
-        //        10.0f,
-        //        "List Adding",
-        //        false
-        //    };
+            List<object> expected = new List<object>()
+            {
+                5.0f,
+                "Test",
+                true,
+                10.0f,
+                "List Adding",
+                false
+            };
 
-        //    CelesteTestUtils.CheckStackResultList(expected);
-        //    CelesteTestUtils.CheckLocalVariableList(script, "addedList", expected);
-        //}
+            CelesteTestUtils.CheckStackResultList(expected);
+            CelesteTestUtils.CheckLocalVariableList(script, "addedList", expected);
+        }
     }
 }
